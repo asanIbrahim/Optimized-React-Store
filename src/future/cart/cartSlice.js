399e6a62f691e8aa9ada4965b498720332c05abb
asanIbrahim <asanib98@gmail.com>
2026-03-10 22:16:16 +0530

    change the values

--- src/future/cart/cartSlice.js
+++ src/future/cart/cartSlice.js
@@ -7,17 +7,15 @@ const cartSlice = createSlice({
   },
   reducers: {
     addCart: (state, action) => {
-
-      const existingItem = state.cartItems.find(
+      const existingItem = state.items.find(
         (item) => item.id === action.payload.id
       );
 
       if (existingItem) {
         existingItem.qty += 1;
       } else {
-        state.cartItems.push({ ...action.payload, qty: 1 });
+        state.items.push({ ...action.payload, qty: 1 });
       }
-
     },
     removeCart: (state, action) => {
       state.items = state.items.filter((val) => val.id !== action.payload);
