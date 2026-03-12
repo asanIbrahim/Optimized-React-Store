674987db9debefd459e5305509f9ddadf958adab
asanIbrahim <asanib98@gmail.com>
2026-03-12 19:42:52 +0530

    add home pages values

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
 
     return (
         <nav style={{ display: "flex", gap: "20px", padding: "10px", background: "#eee" }}>
-            <Link to={'/'}> Home</Link>
+            <Link to={'/'}> Homes</Link>
             <Link to="/cart"> Cart ({cartLength})</Link>
             <Link to="/checkout"> Checkout pages </Link>
         </nav>
