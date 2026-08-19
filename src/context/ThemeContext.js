cabdfc7c151a5918875ef701ce8a31e95647c3f3
Asan M <asan_ibrahim@onwardgroup.com>
2026-08-19 21:32:13 +0530

    theme context

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext()
+
+export function ThemeProvider({children}){
+const[theme,setTheme] = useState("light")
+
+const toggleTheme = ()=>{
+    setTheme((pre) => (pre === "light" ? "dark" : "light"))
+}
+return (
+<ThemeContext.Provider value={{theme,toggleTheme}}> 
+    {children}
+</ThemeContext.Provider>
+)
+}
+
+export function useTheme(){
+    return  useContext(ThemeContext)
+}
