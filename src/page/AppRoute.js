f6538d3fa8c2e77d073a0cea8921005aac0ac341
Asan M <asan_ibrahim@onwardgroup.com>
2026-09-01 20:45:33 +0530

    add

--- src/page/AppRoute.js
+++ src/page/AppRoute.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import TrafficLight from '../component/Trafiic/TrafficLight'
+import Table from '../component/table/Table'
+import Count from '../component/Count/Count'
+import NewTheme from './../component/theme/NewTheme';
 
 export default function AppRoute() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/about" element={<TrafficLight />} />
+        <Route path="/table" element={<Table/>}/>
+        <Route path='/count'  element={<Count/>}/>
+        <Route path = '/theme' element={<NewTheme/>}/>
       </Routes>
     </BrowserRouter>
   )
